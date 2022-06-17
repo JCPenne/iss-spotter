bc98896cd7bcab01d7349d2ff5b647c3b642b989
JCPenne <jackcpalmer@icloud.com>
2022-06-17 12:36:38 -0700

    add function to find geo location based off ip address and test cases in index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const myIP = '23.16.35.123';
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -6,4 +7,11 @@ fetchMyIP((error, ip) => {
     return;
   }
   console.log(`It worked! Returned IP: ${ip}`);
+});
+fetchCoordsByIP((error, GeoCoords) => {
+  if (error) {
+    console.log(`It didn't work! ${error}`);
+    return;
+  }
+  console.log('It worked! Returned GeoCoords: ', GeoCoords);
 });
--- iss.js
+++ iss.js
@@ -1,2 +1,34 @@
-const fetchMyIP = callback => {};
-module.exports = { fetchMyIP };
+const request = require('request');
+
+const fetchMyIP = callback => {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
+const fetchCoordsByIP = (callback, ip) => {
+  request(
+    'https://api.ipbase.com/v2/info?apikey=wW5xnPAH4CDovM15CyKoDs0KZTcfF7OnJ0W9MAJO&ip=23.16.35.123',
+    (error, response, body) => {
+      if (error) return callback(error, null);
+      if (response.statusCode !== 200) {
+        callback(Error(`Status Code ${response.statusCode} when fetching GeoCoords: ${body}`), null);
+        return;
+      }
+      const geoCoords = {
+        latitude: JSON.parse(body).data.location.latitude,
+        longitude: JSON.parse(body).data.location.longitude,
+      };
+      callback(null, geoCoords);
+    },
+  );
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
