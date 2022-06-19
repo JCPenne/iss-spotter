880cc6139443f05445b043bfb66e3258b87f7690
JCPenne <jackcpalmer@icloud.com>
2022-06-19 10:41:56 -0700

    working version of assignment

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
-const myIP = '23.16.35.123';
+const { nextISSTimesForMyLocation } = require('./iss');
+const exampleIP = '23.16.35.123';
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log(`It didn't work! ${error}`);
+//     return;
+//   }
+//   console.log(`It worked! Returned IP: ${ip}`);
+// });
+// fetchCoordsByIP(exampleIP, (error, coords) => {
+//   if (error) {
+//     console.log(`It didn't work! ${error}`);
+//     return;
+//   }
+//   console.log('It worked! Returned GeoCoords: ', coords);
+// });
+// fetchFlyOvers(exampleCoords, (error, output) => {
+//   if (error) {
+//     console.log(`It didn't work! ${error}`);
+//     return;
+//   }
+//   console.log('It worked! Returned Flyovers: ', output);
+// });
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log(`It didn't work! ${error}`);
-    return;
+const printPassTimes = passTimes => {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds`);
   }
-  console.log(`It worked! Returned IP: ${ip}`);
-});
-fetchCoordsByIP((error, GeoCoords) => {
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log(`It didn't work! ${error}`);
-    return;
+    return console.log(`It didn't work! ${error}`);
   }
-  console.log('It worked! Returned GeoCoords: ', GeoCoords);
+  printPassTimes(passTimes);
 });
--- iss.js
+++ iss.js
@@ -13,11 +13,13 @@ const fetchMyIP = callback => {
     callback(null, ip);
   });
 };
-const fetchCoordsByIP = (callback, ip) => {
+
+const fetchCoordsByIP = (ip, callback) => {
   request(
-    'https://api.ipbase.com/v2/info?apikey=wW5xnPAH4CDovM15CyKoDs0KZTcfF7OnJ0W9MAJO&ip=23.16.35.123',
+    `https://api.ipbase.com/v2/info?apikey=wW5xnPAH4CDovM15CyKoDs0KZTcfF7OnJ0W9MAJO&ip=${ip}`,
     (error, response, body) => {
       if (error) return callback(error, null);
+      
       if (response.statusCode !== 200) {
         callback(Error(`Status Code ${response.statusCode} when fetching GeoCoords: ${body}`), null);
         return;
@@ -31,4 +33,36 @@ const fetchCoordsByIP = (callback, ip) => {
   );
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+const fetchFlyOvers = (coords, callback) => {
+  // console.log(coords);
+  const url = `https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+  request(url, (error, response, body) => {
+    if (error) return callback(error, null);
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Flyovers: ${body}`), null);
+      return;
+    }
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+  });
+};
+
+const nextISSTimesForMyLocation = callback => {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+    fetchCoordsByIP(ip, (error, coords) => {
+      if (error) {
+        return callback(error, null);
+      }
+      fetchFlyOvers(coords, (error, passes) => {
+        if (error) {
+          return callback(error, null);
+        }
+        callback(null, passes);
+      });
+    });
+  });
+};
+module.exports = { nextISSTimesForMyLocation };
